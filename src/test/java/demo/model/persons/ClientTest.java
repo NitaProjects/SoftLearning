66d1f5dac776452005d107389e404ab16764ed88
AshAyala <ashgraunuriacefp@gmail.com>
2025-03-20 17:01:56 +0100

    Ash client

--- src/test/java/demo/model/persons/ClientTest.java
+++ src/test/java/demo/model/persons/ClientTest.java
@@ -1,45 +1,60 @@
 package demo.model.persons;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.internal.matchers.Null;
+
+import demo.exceptions.BuildException;
+import demo.model.products.PhysicalData;
 
 public class ClientTest {
-    @Test
-    void testGetCardNum() {
+   private Client client;
 
+   @BeforeEach
+   void createClient(){
+    try{
+        this.client = Client.getInstantClient("87967494S", "Ash", "ashgraunuriacefp@gmail.com", "Carrer Valencia 20", 650285637, "24738724983292", 
+        "08016", "Barcelona");
+    }catch(BuildException ex){
+        ex.getMessage();
+    }
+   } 
+
+    @Test
+    void testGoodNIF(){
+        int result = client.setNif("29032619H");
+        assertEquals(0, result);
     }
 
     @Test
-    void testGetContact() {
-
+    void testBlankNIF(){
+        int result = client.setNif("    ");
+        assertEquals(-1, result);
     }
 
     @Test
-    void testGetInstantClient() {
-
+    void testNullNIF(){
+        int result = client.setNif(null);
+        assertEquals(-1, result);
     }
 
     @Test
-    void testGetPopulation() {
-
+    void testBadNIF(){
+        int result = client.setNif("29391269H");
+        assertEquals(-22, result);
     }
 
     @Test
-    void testGetZipCode() {
-
+    void testGoodName(){
+        int result = client.setName("David");
+        assertEquals(0, result);
     }
 
     @Test
-    void testSetCardNum() {
-
-    }
-
-    @Test
-    void testSetPopulation() {
-
-    }
-
-    @Test
-    void testSetZipCode() {
-
+    void testNullName(){
+        int result = client.setName(null);
+        assertEquals(-1, result);
     }
 }
